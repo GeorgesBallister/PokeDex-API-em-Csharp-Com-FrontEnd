9748314c8056a3174a71df5c23eea2f6c9eded15
GeorgesB <georgesballister.profissional@gmail.com>
2023-05-08 16:51:12 -0300

    Back End

---
+++ PokeDexAPI - Back End/Pokedex/Controllers/PokeDexController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Entities.Models;
+using Pokedex.Percistence;
+
+namespace Pokedex.Controllers
+{
+    [Route("api/pokedex")]
+    [ApiController]
+    public class PokeDexController : ControllerBase
+    {
+
+        private readonly PokeDexDbContext _context;
+
+        public PokeDexController(PokeDexDbContext context)
+        {
+            _context = context;
+        }
+
+        // api/pokedex GET
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var pokemons = _context.Pokemons.Where(d => !d.IsDeleted).ToList();
+            
+            return Ok(pokemons);
+        }
+
+        // api/pokedex/{ID} GET
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var pokemons = _context.Pokemons.SingleOrDefault(d => d.Id == id);
+            if(pokemons == null)
+            {
+                return NotFound();
+            }
+            return Ok(pokemons);
+        }
+
+        // api/pokedex/ POST
+        [HttpPost]
+        public IActionResult Post(Pokemon pokemon)
+        {
+            _context.Pokemons.Add(pokemon);
+            return CreatedAtAction(nameof(GetById), new { id = pokemon.Id }, pokemon);
+        }
+
+        // api/pokedex/{ID} Put
+        [HttpPut("{id}")]
+        public IActionResult Update(int id,Pokemon input)
+        {
+            var pokemons = _context.Pokemons.SingleOrDefault(d => d.Id == id);
+            if (pokemons == null)
+            {
+                return NotFound();
+            }
+            pokemons.Update(input.Nome, input.Tipo);
+
+            return NoContent();
+
+        }
+
+
+        // api/pokedex/{ID} DELETE
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pokemons = _context.Pokemons.SingleOrDefault(d => d.Id == id);
+            if (pokemons == null)
+            {
+                return NotFound();
+            }
+            pokemons.Delete();
+            return NoContent();
+        }
+    }
+}
---
+++ PokeDexAPI - Back End/Pokedex/Percistence/PokeDexDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Pokedex.Entities.Models;
+
+namespace Pokedex.Percistence
+{
+    public class PokeDexDbContext
+    {
+        public List<Pokemon> Pokemons { get; set; }
+        public PokeDexDbContext() {
+            Pokemons = new List<Pokemon>();
+        }
+    }
+}
